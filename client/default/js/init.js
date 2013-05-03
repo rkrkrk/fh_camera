a779cf103579c515612ae5d2ce740639c9c8c8cd
rkrkrk <fintan.mahon@feedhenry.com>
2013-05-03 17:03:02 +0100

    photo changes6

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -2,6 +2,13 @@ $fh.ready(function() {
 
   $fh.legacy.fh_timeout = 500000;
 
+  var myScroll;
+  // function loaded() {
+  // setTimeout(function () {
+  //   myScroll = new iScroll('#wrapper1');
+  //   }, 100);
+  // }
+  // window.addEventListener('load', loaded, false);
   //show login screen, hide the rest
   $('#loginscreen').show();
   $('#content').hide();
@@ -13,7 +20,7 @@ $fh.ready(function() {
 
 
   assignClicks();
-
+          
   //set up click handlers
   function assignClicks(){
     //remove clicks first and reassign them
@@ -67,42 +74,43 @@ $fh.ready(function() {
   function login() {
     $('#loginscreen').hide();
     $('#content').show();
+    iscroll = new iScroll($('#wrapper'));
     $('#singlephoto').hide();
     $('#uploaded').hide();
   };
 
      // dummy take pic for testing
-  // function takePicture() {
-  //     var img = new Image();
-  //     img.src = 'img/fingerprint40.jpg';
-  //     $('#photo_list').append(img);
-  //     $('#photo_list img').removeClass();
-  //     $('#photo_list img').addClass('fingerphotos');  
-  //     $('.fingerphotos').unbind('click');
-  //     $('.fingerphotos').click(function(e) {
-  //       showPicture(e);
-  //     }); 
-  // };
-
   function takePicture() {
-    navigator.camera.getPicture(function(imageData) {
-      console.log('take pic');
       var img = new Image();
-      img.src = 'data:image/jpeg;base64,' + imageData;
+      img.src = 'img/fingerprint40.jpg';
       $('#photo_list').append(img);
       $('#photo_list img').removeClass();
       $('#photo_list img').addClass('fingerphotos');  
       $('.fingerphotos').unbind('click');
       $('.fingerphotos').click(function(e) {
         showPicture(e);
-      });
-
-    }, function() {
-      //error
-    }, {
-      quality: 10
-    });
+      }); 
   };
+
+  // function takePicture() {
+  //   navigator.camera.getPicture(function(imageData) {
+  //     console.log('take pic');
+  //     var img = new Image();
+  //     img.src = 'data:image/jpeg;base64,' + imageData;
+  //     $('#photo_list').append(img);
+  //     $('#photo_list img').removeClass();
+  //     $('#photo_list img').addClass('fingerphotos');  
+  //     $('.fingerphotos').unbind('click');
+  //     $('.fingerphotos').click(function(e) {
+  //       showPicture(e);
+  //     });
+
+  //   }, function() {
+  //     //error
+  //   }, {
+  //     quality: 10
+  //   });
+  // };
 
 
   function showPicture(e) {
