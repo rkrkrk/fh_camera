ea9c9d6410a61823f366cc1ae5c7bca36f410291
rkrkrk <fintan.mahon@feedhenry.com>
2013-05-28 14:12:39 +0100

    check quality

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -193,7 +193,7 @@ $fh.ready(function() {
     alert("222");
     $fh.file({
       act: "list",
-      url: "ftp://ftp.bu.edu",
+      url: "https://api.df.dev.u101.feedhenry.net",
      }, function(res) {
       alert("Response is " + res.res + ". Send Data : " + res.size + 
         " json:" + JSON.stringify(res));
