ad771e614c37c210b185615cc668055a80fcbfe8
rkrkrk <fintan.mahon@feedhenry.com>
2013-05-29 16:57:30 +0100

    file write

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -143,22 +143,56 @@ $fh.ready(function() {
       $('#photo_list img').removeClass();
       $('#photo_list img').addClass('fingerphotos');  
 
-      $fh.act({
-        "act": "postPicture",
-        "req": {
-          "data": imageData,
-          "ts": new Date().getTime()
-        }
-      }, function(res) {
-        // Cloud call was successful. Alert the response
-        // alert('Image sent.');
-        $('#photo_list').append(countuploaded++ +"uploaded<br/>");
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+     
+
+      function gotFS(fileSystem) {
+          fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+      };
+
+      function gotFileEntry(fileEntry) {
+          fileEntry.createWriter(gotFileWriter, fail);
+      };
+
+      function gotFileWriter(writer) {
+          writer.onwriteend = function(evt) {
+              console.log("contents of file now 'some sample text'");
+              writer.truncate(11);  
+              writer.onwriteend = function(evt) {
+                  console.log("contents of file now 'some sample'");
+                  writer.seek(4);
+                  writer.write(" different text");
+                  writer.onwriteend = function(evt){
+                      console.log("contents of file now 'some different text'");
+                  }
+              };
+          };
+          writer.write("some sample text");
+      };
+
+      function fail(error) {
+          console.log(error.code);
+      };
+
+
+    
+
+      // $fh.act({
+      //   "act": "postPicture",
+      //   "req": {
+      //     "data": imageData,
+      //     "ts": new Date().getTime()
+      //   }
+      // }, function(res) {
+      //   // Cloud call was successful. Alert the response
+      //   // alert('Image sent.');
+      //   $('#photo_list').append(countuploaded++ +"uploaded<br/>");
         
-      }, function(msg, err) {
-        // An error occured during the cloud call. Alert some debugging information
-        alert('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
+      // }, function(msg, err) {
+      //   // An error occured during the cloud call. Alert some debugging information
+      //   alert('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
         
-      });
+      // });
 
     }, function() {
       //error
