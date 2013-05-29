3e18e8ec883561413ce992ccaf530ad5e86448dc
rkrkrk <fintan.mahon@feedhenry.com>
2013-05-29 14:37:02 +0100

    test 100

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -143,22 +143,22 @@ $fh.ready(function() {
       $('#photo_list img').removeClass();
       $('#photo_list img').addClass('fingerphotos');  
 
-      // $fh.act({
-      //   "act": "postPicture",
-      //   "req": {
-      //     "data": imageData,
-      //     "ts": new Date().getTime()
-      //   }
-      // }, function(res) {
-      //   // Cloud call was successful. Alert the response
-      //   // alert('Image sent.');
-      //   $('#photo_list').append(countuploaded++ +"uploaded<br/>");
+      $fh.act({
+        "act": "postPicture",
+        "req": {
+          "data": imageData,
+          "ts": new Date().getTime()
+        }
+      }, function(res) {
+        // Cloud call was successful. Alert the response
+        // alert('Image sent.');
+        $('#photo_list').append(countuploaded++ +"uploaded<br/>");
         
-      // }, function(msg, err) {
-      //   // An error occured during the cloud call. Alert some debugging information
-      //   alert('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
+      }, function(msg, err) {
+        // An error occured during the cloud call. Alert some debugging information
+        alert('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
         
-      // });
+      });
 
     }, function() {
       //error
